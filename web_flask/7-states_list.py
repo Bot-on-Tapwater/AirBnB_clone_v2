6b40ddafe46b056b15254d3e7c2fa9754c5bccb5
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-24 01:20:30 +0300

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Simple flask app"""
+
+if __name__ == '__main__':
+    # import module
+    from flask import Flask, render_template
+    from models import storage
+    from models.state import State
+
+    # create instance of Flask class
+    app = Flask(__name__)
+
+    @app.teardown_appcontext
+    def cleanup_app_context():
+        """Remove SQLAlchemy Session"""
+        storage.close()
+
+    @app.route('/states_list', strict_slashes=False)
+    def list_states():
+        """List all state objects"""
+        # returns dict of all states
+        states = storage.all("State")
+        return render_template("7-states_list.html", states=states)
+
+    app.run(host='0.0.0.0', port=5000)
