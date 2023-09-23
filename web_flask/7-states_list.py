3b3de65973735f8c68b0abc634a97a306aa76dc8
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-24 01:34:40 +0300

    Correct clean up method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     app = Flask(__name__)
 
     @app.teardown_appcontext
-    def cleanup_app_context():
+    def cleanup_app_context(random_arg):
         """Remove SQLAlchemy Session"""
         storage.close()
 
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     def list_states():
         """List all state objects"""
         # returns dict of all states
-        states = storage.all("State")
+        states = storage.all(State)
         return render_template("7-states_list.html", states=states)
 
     app.run(host='0.0.0.0', port=5000)
