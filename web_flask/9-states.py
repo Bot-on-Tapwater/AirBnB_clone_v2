d813340dc27845bcfe43e5d0a6c2ccfcfd88520a
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-24 17:43:37 +0300

    Refactor api routes

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -16,11 +16,21 @@ if __name__ == '__main__':
         """Remove SQLAlchemy Session"""
         storage.close()
 
-    @app.route('/states', strict_slashes=False)
-    @app.route('/states/<id>', strict_slashes=False)
-    def states(id=None):
-        """List all states"""
+    @app.route("/states", strict_slashes=False)
+    def states():
+        """Displays an HTML page with a list of all States.
+
+        States are sorted by name.
+        """
         states = storage.all(State)
-        return render_template("9-states.html", states=states, id=id)
+        return render_template("9-states.html", state=states)
+
+    @app.route("/states/<id>", strict_slashes=False)
+    def states_id(id):
+        """Displays an HTML page with info about <id>, if it exists."""
+        for state in storage.all(State).values():
+            if state.id == id:
+                return render_template("9-states.html", state=state)
+        return render_template("9-states.html")
 
     app.run(host='0.0.0.0', port=5000)
